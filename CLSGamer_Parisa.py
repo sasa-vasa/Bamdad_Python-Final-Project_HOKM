6839697a73151c07c9efca5fe9f9cc139a835b80
sasa-vasa <119777393+sasa-vasa@users.noreply.github.com>
2023-03-03 11:51:32 +0330

    Adding second player sending card method

--- CLSGamer_Parisa.py
+++ CLSGamer_Parisa.py
@@ -83,64 +83,69 @@ class Gamer:
         self.Hand.sort()
         for inst_card in PlayGroundCards:
             suit = inst_card[0:-2]
-            for card in self.Hand:
-                if suit in card:
-                    if self.Hand[card] == 14:
-                        card_to_send = {card: self.Hand[card]}                           
-                        self.Hand.pop(card)
-                        return card_to_send 
-                    else:                               
-                        __temp_list.update({card: self.Hand[card]})
-            if len(__temp_list) > 0:
-                for card in __temp_list:
-                    if __temp_list[card] > PlayGroundCards[inst_card]:
-                        card_to_send = {card: self.Hand[card]}                           
-                        self.Hand.pop(card)
-                        return card_to_send                       
-                card = min(__temp_list, key=__temp_list.get)
-                card_to_send = {card: self.Hand[card]}                           
-                self.Hand.pop(card)
-                return card_to_send
-            else:
-                if self.Trump != suit:
-                    for card in self.Hand:
-                        if self.Trump in card:
+            if suit == self.Trump:
+                for card in self.Hand:
+                    if self.Trump in card:
                             __temp_list_Trumps.update({card: self.Hand[card]}) 
-                    if len(__temp_list_Trumps) > 0: 
-                        card = min(__temp_list_Trumps, key=__temp_list_Trumps.get)
+                if len(__temp_list_Trumps) > 0:
+                    for card in self.Hand:
+                        if __temp_list_Trumps[card] > PlayGroundCards[inst_card]:
+                            card_to_send = {card: self.Hand[card]}                           
+                            self.Hand.pop(card)
+                            return card_to_send
+                    card = min(__temp_list_Trumps, key=__temp_list_Trumps.get)
+                    card_to_send = {card: self.Hand[card]}                           
+                    self.Hand.pop(card)
+                    return card_to_send
+            else:  
+                for card in self.Hand:
+                    if suit in card:
+                        if self.Hand[card] == 14:
+                            card_to_send = {card: self.Hand[card]}                           
+                            self.Hand.pop(card)
+                            return card_to_send 
+                        else:                               
+                            __temp_list.update({card: self.Hand[card]})
+                if len(__temp_list) > 0:
+                    for card in __temp_list:
+                        if __temp_list[card] > PlayGroundCards[inst_card]:
+                            card_to_send = {card: self.Hand[card]}                           
+                            self.Hand.pop(card)
+                            return card_to_send                       
+                    card = min(__temp_list, key=__temp_list.get)
+                    card_to_send = {card: self.Hand[card]}                           
+                    self.Hand.pop(card)
+                    return card_to_send
+                else:
+                    __suit_other1 = 0
+                    __suit_other1_value = 0
+                    __temp_list_other1 = {}
+                    __suit_other2 = 0
+                    __suit_other2_value = 0
+                    __temp_list_other2 = {}
+                    __suits_in_Hand = [x for x in ['Spade', 'Club', 'Diamond', 'Heart'] if x not in [suit, self.Trump]]
+                    for card in self.Hand:
+                        if card[0:2] == __suits_in_Hand[0]:
+                                __suit_other1 +=1
+                                __suit_other1_value += self.Hand[card]
+                                __weight_other1 = __suit_other1_value/__suit_other1
+                                __temp_list_other1.update{card: self.Hand[card]}
+                        else:
+                            __suit_other2 +=1
+                            __suit_other2_value += self.Hand[card]
+                            __weight_other2 = __suit_other2_value/__suit_other2
+                            __temp_list_other2.update{card: self.Hand[card]}
+                    if __weight_other1 >= __weight_other2:
+                        card = min(__temp_list_other1, key=__temp_list_other1.get)
                         card_to_send = {card: self.Hand[card]}                           
                         self.Hand.pop(card)
                         return card_to_send
-                    else:
-                        __suit_other1 = 0
-                        __suit_other1_value = 0
-                        __temp_list_other1 = {}
-                        __suit_other2 = 0
-                        __suit_other2_value = 0
-                        __temp_list_other2 = {}
-                        __suits_in_Hand = [x for x in ['Spade', 'Club', 'Diamond', 'Heart'] if x != suit or self.Trump]
-                        for card in self.Hand:
-                            if card[0:2] == __suits_in_Hand[0]:
-                                    __suit_other1 +=1
-                                    __suit_other1_value += self.Hand[card]
-                                    __weight_other1 = __suit_other1_value/__suit_other1
-                                    __temp_list_other1.update{card: self.Hand[card]}
-                            else:
-                                __suit_other2 +=1
-                                __suit_other2_value += self.Hand[card]
-                                __weight_other2 = __suit_other2_value/__suit_other2
-                                __temp_list_other2.update{card: self.Hand[card]}
-                        if __weight_other1 >= __weight_other2:
-                            card = min(__temp_list_other1, key=__temp_list_other1.get)
-                            card_to_send = {card: self.Hand[card]}                           
-                            self.Hand.pop(card)
-                            return card_to_send
-                        else:                            
-                            card = min(__temp_list_other2, key=__temp_list_other2.get)
-                            card_to_send = {card: self.Hand[card]}                           
-                            self.Hand.pop(card)
-                            return card_to_send
-                        
+                    else:                            
+                        card = min(__temp_list_other2, key=__temp_list_other2.get)
+                        card_to_send = {card: self.Hand[card]}                           
+                        self.Hand.pop(card)
+                        return card_to_send
+                            
 
 
 
